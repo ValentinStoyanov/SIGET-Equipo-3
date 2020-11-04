9d838231160cb2428a1927b98fbf12cebafdf52f
GermanUCLM <73961368+GermanUCLM@users.noreply.github.com>
2020-11-04 20:14:41 +0100

    Add files via upload

---
+++ src/test/java/es/uclm/esi/SigetApplicationTests.java
@@ -0,0 +1,13 @@
+package es.uclm.esi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
