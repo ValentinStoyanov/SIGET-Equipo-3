2b76ba18f707c4b63d3bc4558a745729cef48b80
edilbertopozo <edi.pozo.pozo@gmail.com>
2020-11-05 12:47:25 +0100

    Reunion backend

---
+++ src/main/java/es/uclm/esi/model/Asistente.java
@@ -0,0 +1,25 @@
+package es.uclm.esi.model;
+
+public class Asistente {
+
+	private String usuario;
+	private String estado;
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public Asistente(String usuario, String estado) {
+		super();
+		this.usuario = usuario;
+		this.estado = estado;
+	}
+	
+}
