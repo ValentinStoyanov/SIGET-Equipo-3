2d8cca8789af02250f254472fae8276c67795842
German <German@192.168.1.137>
2020-11-12 16:40:43 +0100

    Asistentes

--- src/main/java/es/uclm/esi/controller/ControllerCalendarioPersonal.java
+++ src/main/java/es/uclm/esi/controller/ControllerCalendarioPersonal.java
@@ -1,36 +1,32 @@
 package es.uclm.esi.controller;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Map;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import es.uclm.esi.repository.RepositoryCalendarioPersonal;
-import es.uclm.esi.repository.UserRepository;
 
 /**
  * 
- * @author Víctor
- * Clase para recepción de mensajes http relacionados con obtener el calendario personal
+ * @author Víctor Clase para recepción de mensajes http relacionados con obtener
+ *         el calendario personal
  */
 @RestController
 public class ControllerCalendarioPersonal {
-	
+
 	@Autowired
 	RepositoryCalendarioPersonal calendarioRepository;
 
 	@PostMapping("/getCalendarioPersonalMes")
-    public String getCalendarioPersonalMes(@RequestBody Map<String, Object> entrada){
-		JSONObject jso=new JSONObject(entrada);
-		System.out.println("Pues mira me ha llegao esto: "+ jso);
+	public String getCalendarioPersonalMes(@RequestBody Map<String, Object> entrada) {
+		JSONObject jso = new JSONObject(entrada);
+		System.out.println("Pues mira me ha llegao esto: " + jso);
 		int mespeticion = jso.getInt("mes");
 		int anopeticion = jso.getInt("ano");
 
@@ -41,32 +37,40 @@ public class ControllerCalendarioPersonal {
 				dias.add(calendarioRepository.findById(wid).getDia());
 				wid++;
 			}
-		} catch (Exception e) {}
+		} catch (Exception e) {
+		}
 		JSONObject jsoret = new JSONObject();
-		
+
 		int[] diasjson = new int[dias.size()];
-		for(int i = 0; i < dias.size(); i++) {
+		for (int i = 0; i < dias.size(); i++) {
 			diasjson[i] = dias.get(i);
 		}
 
 		jsoret.put("dias", diasjson);
-		jsoret.put("mes", calendarioRepository.findById(jso.getInt("mes")==calendarioRepository.findById(1).getMes()? 1 : 2).getMes());
-		jsoret.put("ano", calendarioRepository.findById(1).getAno());
-		
-        return jsoret.toString();
-    }
-	
-	@PostMapping("/getDetallesReunion")
-    public String getDetallesReunion(@RequestBody Map<String, Object> entrada){
-		JSONObject jso=new JSONObject(entrada);
-		System.out.println("Pues mira me ha llegao esto: "+ jso);
-		JSONObject jsoret = new JSONObject();
-		jsoret.put("titulo", calendarioRepository.findById(1));
+		jsoret.put("mes", mespeticion);
+		jsoret.put("ano", anopeticion);
 
-		
-        return calendarioRepository.findById(1).getTitulo();
-    }
-	
-	
+		return jsoret.toString();
+	}
+
+	@PostMapping("/getDetallesReunion")
+	public String getDetallesReunion(@RequestBody Map<String, Object> entrada) {
+		JSONObject jso = new JSONObject(entrada);
+		System.out.println("Pues mira me ha llegao esto: " + jso);
+
+		String[] asistentes = { "vacío" };
+		try {
+			int wid = 1;
+			while (wid>0) {
+				if (calendarioRepository.findById(wid).getDia() == 23
+						&& calendarioRepository.findById(wid).getMes() == 11) {
+					asistentes = calendarioRepository.findById(wid).getAsistentes();
+				}
+				wid++;
+			}
+		} catch (Exception e) {
+		}
+		return calendarioRepository.findById(1).getTitulo();
+	}
 
 }
