91eb647767442ff6e0f3c1d3235fe8bf4b231ba4
edilbertopozo <edi.pozo.pozo@gmail.com>
2020-11-12 19:04:31 +0100

    titulo2

--- src/test/java/es/uclm/esi/ModificarReunionStepDefinitions.java
+++ src/test/java/es/uclm/esi/ModificarReunionStepDefinitions.java
@@ -31,9 +31,11 @@ public class ModificarReunionStepDefinitions extends SpringIntegrationTest {
     HttpHeaders headers = new HttpHeaders();
     Reunion reu;
     Asistente[] arrayAsistentes;
+    int numReunion;
     
     @When("Modifico la reunion {int} con el token de usuario {string}")
     public void modifico_la_reunion_con_el_token_de_usuario(Integer int1, String string) {
+    	numReunion = int1;
     	Optional<Reunion> r = rcp.findById(int1.toString());
     	
     	if(r.isPresent()) reu = r.get();
@@ -94,13 +96,21 @@ public class ModificarReunionStepDefinitions extends SpringIntegrationTest {
     }
     @Then("el nuevo titulo sera {string}")
     public void el_nuevo_titulo_sera(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    	Optional<Reunion> r = rcp.findById(String.valueOf(numReunion));
+    	if(r.isPresent()) reu = r.get();
+    	if(!string.equals("")) {
+    		assertEquals(reu.getTitulo(), string);
+    	}
+    	
     }
     @Then("la nueva fecha sera {string}")
     public void la_nueva_fecha_sera(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    	if(!string.equals("")) {
+//    		reu.setDia(Integer.parseInt(string.substring(0, 2)));
+//        	reu.setMes(Integer.parseInt(string.substring(3, 5)));
+//        	reu.setAno(Integer.parseInt(string.substring(6, 10)));
+//    		assertEquals(reu.getTitulo(), string);
+    	}
     }
     @Then("la nueva hora sera {string}")
     public void la_nueva_hora_sera(String string) {
