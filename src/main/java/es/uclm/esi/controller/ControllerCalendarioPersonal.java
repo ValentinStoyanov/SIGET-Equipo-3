78a56b056390ee8222f91b2f7752c50e334d4aa7
German <German@172.19.132.104>
2020-11-12 12:23:45 +0100

    Cambio repository, funcional

--- src/main/java/es/uclm/esi/controller/ControllerCalendarioPersonal.java
+++ src/main/java/es/uclm/esi/controller/ControllerCalendarioPersonal.java
@@ -29,63 +29,19 @@ public class ControllerCalendarioPersonal {
 	@PostMapping("/getCalendarioPersonalMes")
     public String getCalendarioPersonalMes(@RequestBody Map<String, Object> entrada){
 		JSONObject jso=new JSONObject(entrada);
-		//System.out.println("Pues mira me ha llegao esto: "+ jso);
-		JSONObject jsoret = new JSONObject();
-		jsoret.put("mes", 11);
-		jsoret.put("ano",2020);
-		int[] dias = {05,12,17,21,26};
-		jsoret.put("reuniones", dias);
-		
+		System.out.println("Pues mira me ha llegao esto: "+ jso);
 		System.out.println(calendarioRepository.findById(1).getTitulo());
 		
-        return jsoret.toString();
+        return calendarioRepository.findById(1).getTitulo();
     }
 	
 	@PostMapping("/getDetallesReunion")
     public String getDetallesReunion(@RequestBody Map<String, Object> entrada){
 		JSONObject jso=new JSONObject(entrada);
-		//System.out.println("Pues mira me ha llegao esto: "+ jso);
-		String[] asistentes = {"Jesus Cabanero","Victor Ramirez","German Rolando","Fatima Sanchez","Angel Villaseñor","Edilberto Pozo"};
-		JSONObject jsoret = new JSONObject();
-		jsoret.put("dia", 26);
-		jsoret.put("mes", 11);
-		jsoret.put("ano", 2020);
+		System.out.println("Pues mira me ha llegao esto: "+ jso);
+
 		
-		JSONArray jsa = new JSONArray();	
-		JSONObject Inforeu1 = new JSONObject();
-		JSONObject Inforeu2 = new JSONObject();
-		JSONObject Inforeu3 = new JSONObject();
-		
-		Inforeu1.put("titulo", "Frontend");
-		Inforeu1.put("id", 1);
-		Inforeu1.put("hora", "11:30");
-		Inforeu1.put("duracion", 90);
-		Inforeu1.put("asistentes", asistentes);
-		Inforeu1.put("descripcion", "Una reunion para determinar el diseno del frontend"); 
-		
-		Inforeu2.put("titulo", "Daily");
-		Inforeu2.put("id", 2);
-		Inforeu2.put("hora", "22:00");
-		Inforeu2.put("duracion", 15);
-		Inforeu2.put("asistentes", asistentes);
-		Inforeu2.put("descripcion", "Reunion daily para comentar el estado del proyecto");
-		
-		Inforeu3.put("titulo", "Revisión del sprint");
-		Inforeu3.put("id", 3);
-		Inforeu3.put("hora", "16:50");
-		Inforeu3.put("duracion", 120);
-		Inforeu3.put("asistentes", asistentes);
-		Inforeu3.put("descripcion", "Reunion para ver los resultados del sprint");
-		
-		jsa.put(Inforeu1);
-		jsa.put(Inforeu2);
-		jsa.put(Inforeu3);
-		
-		jsoret.put("reuniones", jsa);
-		
-		//System.out.println(jsoret);
-		
-        return jsoret.toString();
+        return calendarioRepository.findById(1).getTitulo();
     }
 	
 	
