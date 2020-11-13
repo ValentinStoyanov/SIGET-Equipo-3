817fefa3d08748c0e37fd976f0e9126d60a2e0a7
JESUS <USUARIO@DESKTOP-7NJHQ92>
2020-11-13 15:41:22 +0100

    Creación de las clases StepDefinitions de los escenarios de consultarReunionesDia y consulttarinfoReunion

---
+++ src/test/java/es/uclm/esi/ConsultarInfoReunionStepDefinitions.java
@@ -0,0 +1,39 @@
+package es.uclm.esi;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ConsultarInfoReunionStepDefinitions {
+
+	@Given("el usuario quiere ver la información de la reunion seleccionada")
+	public void el_usuario_quiere_ver_la_información_de_la_reunion_seleccionada() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("el titulo de la reunion es {string}")
+	public void el_titulo_de_la_reunion_es(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("la hora de la reunion es {string}")
+	public void la_hora_de_la_reunion_es(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("los asistentes son {string},{string}")
+	public void los_asistentes_son(String string, String string2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("los asistentes son {string}")
+	public void los_asistentes_son(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+}
---
+++ src/test/java/es/uclm/esi/ConsultarReunionesDiaStepDefinitions.java
@@ -0,0 +1,33 @@
+package es.uclm.esi;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ConsultarReunionesDiaStepDefinitions {
+
+	@Given("el usuario quiere ver las reuniones de un dia")
+	public void el_usuario_quiere_ver_las_reuniones_de_un_dia() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("Muestro la lista de reuniones del dia {int}")
+	public void muestro_la_lista_de_reuniones_del_dia(Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Muestro el codigo {int}")
+	public void muestro_el_codigo(Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("Muestro la lista de reuniones del dia {string}")
+	public void muestro_la_lista_de_reuniones_del_dia(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+}
