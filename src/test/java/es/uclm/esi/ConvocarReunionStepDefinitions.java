77171d23f3e6778a06a0ea87a5d561005d6be7fd
Maria-Fatima <MFatima.Sanchez@alu.uclm.es>
2020-11-13 17:16:55 +0100

    Añadida Step Definitions

---
+++ src/test/java/es/uclm/esi/ConvocarReunionStepDefinitions.java
@@ -0,0 +1,80 @@
+package es.uclm.esi;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ConvocarReunionStepDefinitions {
+
+@Given("el organizador quiere convocar una reunion")
+public void el_organizador_quiere_convocar_una_reunion() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@When("convoco la reunion {int}")
+public void convoco_la_reunion(Integer int1) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@When("organizador es {string}")
+public void organizador_es(String string) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@When("titulo es {string}")
+public void titulo_es(String string) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@When("estado es {string}")
+public void estado_es(String string) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@When("dia es {int}")
+public void dia_es(Integer int1) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@When("mes es {int}")
+public void mes_es(Integer int1) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@When("ano es {int}")
+public void ano_es(Integer int1) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@When("hora es <String>")
+public void hora_es_string() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@When("asistentes son {string},{string},{string}")
+public void asistentes_son(String string, String string2, String string3) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@Then("la respuesta sera {int}")
+public void la_respuesta_sera(Integer int1) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@When("asistentes son {string}")
+public void asistentes_son(String string) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+}
