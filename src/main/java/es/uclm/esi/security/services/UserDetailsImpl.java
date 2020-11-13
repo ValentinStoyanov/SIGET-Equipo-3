e8ec1143987754af8e83156f1db3a609b28ce381
German <German@172.19.179.40>
2020-11-13 12:56:47 +0100

    Eliminado bug UserDetailsImpl

--- src/main/java/es/uclm/esi/security/services/UserDetailsImpl.java
+++ src/main/java/es/uclm/esi/security/services/UserDetailsImpl.java
@@ -2,7 +2,6 @@ package es.uclm.esi.security.services;
 
 import java.util.Collection;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 import org.springframework.security.core.GrantedAuthority;
@@ -93,13 +92,5 @@ public class UserDetailsImpl implements UserDetails {
 		return true;
 	}
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o)
-			return true;
-		if (o == null || getClass() != o.getClass())
-			return false;
-		UserDetailsImpl user = (UserDetailsImpl) o;
-		return Objects.equals(id, user.id);
-	}
+
 }
