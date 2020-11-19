21dc54c08a6e6176d2fc08f098c1a9d1ccd40a5a
edilbertopozo <edi.pozo.pozo@gmail.com>
2020-11-19 17:54:59 +0100

    falta borrar los nuevos creados

--- src/test/java/es/uclm/esi/RegistroStepDefinitions.java
+++ src/test/java/es/uclm/esi/RegistroStepDefinitions.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
@@ -16,19 +17,25 @@ import org.springframework.web.client.HttpClientErrorException;
 import es.uclm.esi.payload.request.CalendarioDiaRequest;
 import es.uclm.esi.payload.request.CalendarioMesRequest;
 import es.uclm.esi.payload.request.SignupRequest;
+import es.uclm.esi.repository.UserRepository;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class RegistroStepDefinitions extends SpringIntegrationTest {
-
+	
+	@Autowired
+	UserRepository userR;
+	
 	ResponseEntity<String> response;
 	String url = DEFAULT_URL + "api/auth/signup/";
 	SignupRequest request;
 	boolean borrar = false;
+	String nuevo;
 
 	@When("registro un usuario con usuario {string}, pass {string} y email {string}")
 	public void registro_un_usuario_con_usuario_pass_y_email(String string, String string2, String string3) {
 	    request = new SignupRequest();
+	    nuevo = string;
 	    request.setUsername(string);
 	    request.setPassword(string2);
 	    request.setEmail(string3);
@@ -46,10 +53,20 @@ public class RegistroStepDefinitions extends SpringIntegrationTest {
 		try {
 			response = restTemplate.postForEntity(url, hrequest, String.class);
 			
-			assertEquals(string, response.getBody());
+			assertEquals(string, response.getBody().substring(12, response.getBody().length()-2));
+			userR.deleteByUsername(nuevo);
 		} catch (HttpClientErrorException e) {
-			assertEquals(string, e.getMessage());
+			
+			assertEquals(string, e.getMessage().substring(19, e.getMessage().length()-3));
+				
+			
 		}
+		//borro para que no falle despues
+		if(borrar) {
+			
+			userR.deleteByUsername(nuevo);
+		}
+			
 	}
 
 }
