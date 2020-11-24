331c120af58748e56f4efa7db39adaeda1bb3cbb
GermanUCLM <German.Rolando@alu.uclm.es>
2020-11-24 10:45:24 +0100

    Bad Smells AuthController y ConvocarController

--- src/main/java/es/uclm/esi/controller/AuthController.java
+++ src/main/java/es/uclm/esi/controller/AuthController.java
@@ -90,31 +90,6 @@ public class AuthController {
 		User user = new User(signUpRequest.getUsername(), 
 							 signUpRequest.getEmail(),
 							 encoder.encode(signUpRequest.getPassword()));
-
-		
-//		Set<Role> roles = new HashSet<>();
-//
-//		if (strRoles == null) {
-//			Role userRole = roleRepository.findByName(ERole.ROLE_USER)
-//					.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-//			roles.add(userRole);
-//		} else {
-//			strRoles.forEach(role -> {
-//				switch (role) {
-//				case "admin":
-//					Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
-//							.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-//					roles.add(adminRole);
-//
-//					break;
-//				
-//				default:
-//					Role userRole = roleRepository.findByName(ERole.ROLE_USER)
-//							.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-//					roles.add(userRole);
-//				}
-//			});
-//		}
 		
 		//lo de arriba lo comento ya que solo se registra el usuario como USER y el rol solo se lo puede asignar un admin en el panel (no implementado)
 		Set<Role> roles = new HashSet<>();
